a53895664ec5bc63accf69a498f0aba87012031e
katerynak <konotopska.k@gmail.com>
2020-12-13 20:24:55 +0100

    Comment out image rebuilding.

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
 # Eliminate all the previous containers & custom images  and rebuilds everything.
 
 # For now we assume to have only one container.
-docker container rm `docker container ls -a | grep crawler | head -c 6`
-docker container rm `docker container ls -a | grep broker | head -c 6`
-docker container rm `docker container ls -a | grep client | head -c 6`
-docker image rm crawler
-docker image rm broker
-docker image rm client
+#docker container rm `docker container ls -a | grep crawler | head -c 6`
+#docker container rm `docker container ls -a | grep broker | head -c 6`
+#docker container rm `docker container ls -a | grep client | head -c 6`
+#docker image rm crawler
+#docker image rm broker
+#docker image rm client
 
 docker-compose up
