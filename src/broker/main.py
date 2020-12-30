546617bedb2040e998c72e80a9a6819e6f2994a9
katerynak <konotopska.k@gmail.com>
2020-12-30 12:24:25 +0100

    Redirect messagesto crawler only when there are no new entries in the db.

--- src/broker/main.py
+++ src/broker/main.py
@@ -4,22 +4,73 @@ needed, and sends them to the django client.
 """
 import ast
 import os
+import datetime
 
 import mongoengine as mongo
+import mongoengine.queryset.visitor as mongo_visitor
 import redis
 
-import data_sorting.product_sorter as product_sorter
+import data_collections.Product as Product
+import data_collections.ProductPrice as ProductPrice
+
 
 # Environment variables that should be set.
 ENVIRONMENT_VARS = [
-    "CRAWLER_OUTPUT_QUEUE",
+    "CRAWLER_PRODUCTS_INPUT_QUEUE",
     "BROKER_PRODUCTS_OUTPUT_QUEUE",
+    "BROKER_PRODUCTS_INPUT_QUEUE",
     "MONGODB_NAME",
     "REDIS_HOST",
     "REDIS_PORT",
     "MONGODB_HOST",
     "MONGODB_PORT",
 ]
+
+
+def wait_for_input():
+    """
+    Loop waiting for the user input.
+
+    Loop waiting for a message with the user input from the `redis` queue.
+    When an input is received, exits the loop and returns the received message.
+
+    :return: the received message from the `redis` queue
+    """
+    # Flag showing whether the input was found.
+    found_input = False
+    # Loop until receive a message.
+    while not found_input:
+        print("Waiting for an input.")
+        # Check for a new message from the redis queue.
+        task = redis_connection.blpop(redis_queues, 0)
+        received_queue = task[0].decode("utf-8")
+        received_input = task[1].decode("utf-8")
+        print(f"Received message from {received_queue}")
+        found_input = True
+    return received_input
+
+
+def present_in_db(search_term, time_delta):
+    """
+    Checks if a product is present in the database.
+
+    :param search_term: user search term
+    :param time_delta: used to decide whether an entry is "old" or not
+    :return: True if there are recent entries in the db related to the search
+        term, False otherwise
+    """
+    # Look for product into the db.
+    product_prices = ProductPrice.ProductPrice.objects(
+        (mongo_visitor.Q(search_term=search_term))
+    ).order_by('date')
+    if len(product_prices) == 0:
+        return False
+    last_insertion_datetime = product_prices[0].date
+    current_datetime = datetime.datetime.now()
+    if current_datetime - last_insertion_datetime > time_delta:
+        return False
+    return True
+
 
 if __name__ == "__main__":
     # Check if the environment variables are defined.
@@ -28,44 +79,33 @@ if __name__ == "__main__":
             "%s environment variable should be defined." % env_var
         )
     # Connect to the redis instance.
-    print("connecting to redis...")
-    redis_c = redis.Redis(
+    print("Connecting to redis...")
+    redis_connection = redis.Redis(
         host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"], charset="utf-8"
     )
-    print("connected to redis")
+    print("Connected to redis.")
     # Connect to the database.
-    print("creating connection to mongodb")
+    print("Creating connection to mongodb.")
     mongo.connect(
         os.environ["MONGODB_NAME"],
         host=os.environ["MONGODB_HOST"],
         port=int(os.environ["MONGODB_PORT"]),
     )
-    print("connection to mongodb created")
+    print("Connection to mongodb created.")
     # Set up redis to wait for messages.
     redis_queues = []
-    for queue in ["CRAWLER_OUTPUT_QUEUE"]:
+    # Listen to the input queue
+    for queue in ["BROKER_PRODUCTS_INPUT_QUEUE"]:
         redis_queues.append(os.environ[queue])
-    # TODO: remove this when no more recessary.
     # For debugging purpose only, clear the redis cache.
-    redis_c.flushall()
-    # Initialize product sorter.
-    sorter = product_sorter.ProductSorter("memes")
+    redis_connection.flushall()
     # Repeat this cycle.
     while True:
-        # Check for a new message from the redis queue.
-        task = redis_c.blpop(redis_queues, 0)
-        received_queue = task[0].decode("utf-8")
-        received_input = task[1].decode("utf-8")
-        print("Received message from %s" % received_queue)
-        # Parse the received input.
-        try:
-            received_input = ast.literal_eval(received_input)
-            if received_queue == os.environ["CRAWLER_OUTPUT_QUEUE"]:
-                print("Received {}".format(received_input))
-            # Send the received input info to the client.
-            # TODO: send to the client.
-        except SyntaxError:
-            print(
-                f"Data from {received_queue} not a valid python dict, "
-                f"discarding:\n {received_input}"
+        # Wait for the user input.
+        new_search_term = wait_for_input()
+        # Check if there are recent entries for the search term.
+        crawl = not present_in_db(new_search_term, datetime.timedelta(minutes=15))
+        if crawl:
+            redis_connection.lpush(
+                os.environ["CRAWLER_PRODUCTS_INPUT_QUEUE"], new_search_term
             )
