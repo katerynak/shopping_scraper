95528674caa0bc81f457fe92484d248736c6cb4b
katerynak <konotopska.k@gmail.com>
2020-12-30 21:02:30 +0100

    Change ah id data type.

--- data_collections/Product.py
+++ data_collections/Product.py
@@ -7,7 +7,6 @@ class Product(mongo.DynamicDocument):
     """
     Definition of product schema.
     """
-
     name = mongo.StringField(required=True, max_length=100)
     search_term = mongo.ListField(required=False)
     last_update = mongo.DateTimeField(default=datetime.datetime.utcnow)
@@ -19,12 +18,11 @@ class Product(mongo.DynamicDocument):
     ah_link = mongo.URLField(required=False)
     # shop_ids =  ListField(required=False)
     coop_id = mongo.IntField(required=False)
-    ah_id = mongo.IntField(required=False)
+    ah_id = mongo.StringField(required=False)
     ah_image = mongo.BinaryField(required=False)
     coop_image = mongo.BinaryField(required=False)
     ah_name = mongo.StringField(required=False, max_length=100)
     coop_name = mongo.StringField(required=False, max_length=100)
     ah_ranking = mongo.IntField(required=False)
     coop_ranking = mongo.IntField(required=False)
-    # brand =  StringField(required=False, max_length=50)
-    # ingredients =  ListField(required=False)
+
